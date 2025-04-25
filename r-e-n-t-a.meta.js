df44653307c7fb75d2b6aa88abb1a08ef9bc9419
kawasakisora2345 <kawasakisora2345@github.com>
2025-04-25 10:08:34 +0700

    init

--- r-e-n-t-a.meta.js
+++ r-e-n-t-a.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         r-e-n-t-a
 // @namespace    r-e-n-t-a.user.js
-// @version      2025.04.17
+// @version      2025.04.25
 // @description  down r-e-n-t-a
 // @author       You
 // @include      /^https://dre-viewer.papy.co.jp/sc/.+\/index\.view/
 // @icon         https://raw.githubusercontent.com/kawasakisora2345/vao_mot_ta/main/r-e-n-t-a.png
 // @grant        none
-// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @downloadURL  https://github.com/kawasakisora2345/vao_mot_ta/raw/main/r-e-n-t-a.user.js
 // @updateURL    https://github.com/kawasakisora2345/vao_mot_ta/raw/main/r-e-n-t-a.meta.js
 // ==/UserScript==
